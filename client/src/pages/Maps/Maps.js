bee371f78521bc494b278aac3d4a6cc6a509a127
WLeondike <billyleondike@gmail.com>
2020-08-17 13:36:58 -0400

    minor changes

--- client/src/pages/Maps/Maps.js
+++ client/src/pages/Maps/Maps.js
@@ -7,28 +7,28 @@ import {
 } from "@react-google-maps/api";
 
 const mapContainerStyle = {
-    width: "30vw",
-    height: "40vh"
+  width: "30vw",
+  height: "40vh",
 };
 
 const center = {
-    lat: 28.538336,
-    lng: -81.379234
+  lat: 28.538336,
+  lng: -81.379234,
 };
 
 const Map = () => {
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_API_KEY
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
   });
 
   if (loadError) return "Error loading maps";
-  if (!isLoaded) return "Loading Maps";
+  if (!isLoaded) return "Loading google maps";
 
   return (
     <div>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
-        zoom={ 12 }
+        zoom={12}
         center={center}
       />
     </div>
