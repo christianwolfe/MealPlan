dea3c759a6eb1ce812bb503966fcb80f113e724a
WLeondike <billyleondike@gmail.com>
2020-08-18 14:57:15 -0400

    updated and switched map npm package

---
+++ client/src/pages/Map/Map.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} from "react-google-maps";
+
+const GoogMap = () => {
+  return (
+    <GoogleMap
+      defaultZoom={12}
+      defaultCenter={{ lat: 28.538336, lng: -81.379234 }}
+    />
+  );
+};
+
+const MapWrapped = withScriptjs(withGoogleMap(GoogMap));
+
+const Map = () => {
+  return (
+    <div style={{ width: "50vw", height: "50vw" }}>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY}`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      >
+      </MapWrapped>
+    </div>
+  );
+};
+
+export default Map;
