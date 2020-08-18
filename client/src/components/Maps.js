2aefbd4c5395814a102e0ebea8622056db66c8b1
WLeondike <billyleondike@gmail.com>
2020-08-18 15:23:14 -0400

    working on markers for map on home page

--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -6,14 +6,24 @@ import {
   Marker,
   InfoWindow,
 } from "react-google-maps";
-import * as bankData from "../data/orlando-food-banks.json"
+import * as bankData from "../data/orlando-food-banks.json";
 
 const GoogMap = () => {
   return (
     <GoogleMap
       defaultZoom={12}
       defaultCenter={{ lat: 28.538336, lng: -81.379234 }}
-    />
+    >
+      {bankData.features.map((bank) => (
+        <Marker
+          key={bank.properties.BANK_ID}
+          position={{
+            lat: bank.geometry.coordinates[1],
+            lng: bank.geometry.coordinates[0],
+          }}
+        />
+      ))}
+    </GoogleMap>
   );
 };
 
@@ -21,15 +31,13 @@ const MapWrapped = withScriptjs(withGoogleMap(GoogMap));
 
 const Map = () => {
   return (
-    <div style={{ width: "50vw", height: "50vw" }}>
+    <div style={{ width: "25vw", height: "25vw" }}>
       <MapWrapped
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY}`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
-      >
-
-      </MapWrapped>
+      />
     </div>
   );
 };
