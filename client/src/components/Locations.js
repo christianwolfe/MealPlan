dfdd08109608c9fade6f5bb2ee03f7996144c5ce
Christian Wolfe <cwolfe.k@gmail.com>
2020-08-19 14:05:57 -0400

    accordion works again

--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -1,22 +1,62 @@
 import React, { Component } from 'react';
-import * as Data from "../data/orlando-food-banks.json";
+import * as Banks from "../data/orlando-food-banks.json";
+import { Accordion, Icon } from 'semantic-ui-react'
 
 // class Locations extends Component { 
 
 //why dont we need render(){?
 // client/src/index.js?  
 
-export default function Locations() { 
- console.log(Data)
-    return (
-        <div className = "container">
-                { Data.features.map(data => {
-                    return (
-                        <h4>{ data.properties.NAME }</h4>
-                    )
-                })}
-            </div>
-    )}
+function checkWeb() {
+    
+}
+
+export default class AccordionStyled extends Component {
+
+    state = { activeIndex: 0, results: Banks.default.features }
+
+    handleClick = (e, titleProps) => {
+        console.log(titleProps)
+        const { index } = titleProps
+        const { activeIndex } = this.state  //if active index is equal to index, i want to return -1,
+        const newIndex = activeIndex === index ? -1 : index
+
+        this.setState({ activeIndex: newIndex })
+    }
+    fetchData = () => {
+        //api call to GET all info from /data/, then set state of results to whatever is being retrieved
+    }
+    render() {
+        console.log(Banks.default.features)
+        const { activeIndex } = this.state
+        return (
+            this.state.results.map((data, i) => {
+                return (
+
+                    <Accordion styled>
+                        <Accordion.Title
+                            active={activeIndex === i}
+                            index={i}
+                            onClick={this.handleClick}
+                        >
+                            <Icon name='dropdown' />
+                            {data.properties.NAME}
+                        </Accordion.Title>
+                        <Accordion.Content active={activeIndex === i}>
+                            <p> <Icon name="map pin" />{data.properties.ADDRESS}</p>
+                            <p> <Icon name="phone" /> {data.properties.PHONE}</p>
+                            <Icon name="globe" /><a href={data.properties.WEBSITE} target="_blank">{data.properties.WEBSITE}</a>
+                            <hr/>
+                            <p>
+                                {data.properties.DESCRIPTION}
+                            </p>
+                        </Accordion.Content>
+                    </Accordion>
+                )
+            })
+        )
+    };
+};
 
 
-
+    //cannot wrap render or return of the component in to a function
