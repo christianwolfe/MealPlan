61541ca13a0a1c7478a0527a0208412396c51fe9
WLeondike <billyleondike@gmail.com>
2020-08-19 15:43:55 -0400

    added modal, needs work

--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -1,62 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import * as Banks from "../data/orlando-food-banks.json";
-import { Accordion, Icon } from 'semantic-ui-react'
-
-// class Locations extends Component { 
-
-//why dont we need render(){?
-// client/src/index.js?  
-
-function checkWeb() {
-    
-}
+import { Accordion, Icon } from "semantic-ui-react";
+import Modal from "./Modal";
 
 export default class AccordionStyled extends Component {
+  state = { activeIndex: 0, results: Banks.default.features };
 
-    state = { activeIndex: 0, results: Banks.default.features }
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state; //if active index is equal to index, i want to return -1,
+    const newIndex = activeIndex === index ? -1 : index;
+    this.setState({ activeIndex: newIndex });
+  };
 
-    handleClick = (e, titleProps) => {
-        console.log(titleProps)
-        const { index } = titleProps
-        const { activeIndex } = this.state  //if active index is equal to index, i want to return -1,
-        const newIndex = activeIndex === index ? -1 : index
-
-        this.setState({ activeIndex: newIndex })
-    }
-    fetchData = () => {
-        //api call to GET all info from /data/, then set state of results to whatever is being retrieved
-    }
-    render() {
-        console.log(Banks.default.features)
-        const { activeIndex } = this.state
-        return (
-            this.state.results.map((data, i) => {
-                return (
-
-                    <Accordion styled>
-                        <Accordion.Title
-                            active={activeIndex === i}
-                            index={i}
-                            onClick={this.handleClick}
-                        >
-                            <Icon name='dropdown' />
-                            {data.properties.NAME}
-                        </Accordion.Title>
-                        <Accordion.Content active={activeIndex === i}>
-                            <p> <Icon name="map pin" />{data.properties.ADDRESS}</p>
-                            <p> <Icon name="phone" /> {data.properties.PHONE}</p>
-                            <Icon name="globe" /><a href={data.properties.WEBSITE} target="_blank">{data.properties.WEBSITE}</a>
-                            <hr/>
-                            <p>
-                                {data.properties.DESCRIPTION}
-                            </p>
-                        </Accordion.Content>
-                    </Accordion>
-                )
-            })
-        )
-    };
-};
-
-
-    //cannot wrap render or return of the component in to a function
+  render() {
+    const { activeIndex } = this.state;
+    return this.state.results.map((data, i) => {
+      return (
+        <Accordion styled>
+          <Accordion.Title
+            active={activeIndex === i}
+            index={i}
+            onClick={this.handleClick}
+          >
+            <Icon name="dropdown" />
+            {data.properties.NAME}
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === i}>
+            <p>
+              {" "}
+              <Icon name="map pin" />
+              {data.properties.ADDRESS}
+            </p>
+            <p>
+              {" "}
+              <Icon name="phone" /> {data.properties.PHONE}
+            </p>
+            <Icon name="globe" />
+            <a href={data.properties.WEBSITE} target="_blank">
+              {data.properties.WEBSITE}
+            </a>
+            <hr />
+            <p>{data.properties.DESCRIPTION}</p>
+            <Modal />
+          </Accordion.Content>
+        </Accordion>
+      );
+    });
+  }
+}
